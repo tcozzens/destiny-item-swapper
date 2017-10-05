9f138602157ffa3bf757dc5e1a47dc7aae29b2ac
tcozzens <tcozzens91@gmail.com>
2017-10-04 20:38:22 -0400

    Making some API calls

--- index.js
+++ index.js
@@ -1,94 +1,161 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
-
-'use strict';
-
-const Alexa = require('alexa-sdk');
-var request = require('request');
-
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
-const API_KEY = '461360109a974a24a9f07e54653a5001';
-
-const languageStrings = {
-    'en': {
-        translation: {
-            SKILL_NAME: 'Destiny Item Swapper',
-            GET_FACT_MESSAGE: "Hang on a second...",
-            STOP_MESSAGE: 'Goodbye!',
-            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            TUCKER_TEST: 'tuckers test',
-            BETTER_GEAR: 'HAHAHA! Is this a joke? Clearly dj dipmode is an r. n. g. god.'
-        },
-    },
-};
-
-const handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetItem');
-    },
-    'GetNewFactIntent': function () {
-        this.emit('GetItem');
-    },
-    'TuckerTest': function () {
-        // const speechOutput = this.t('TUCKER_TEST');
-        // this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'));
-        var urlForMembership = 'https://bungie.net/Platform/User/SearchUsers/?q=alltuckerdout';
-        let options = { url: urlForMembership, headers: {'X-API-Key': API_KEY}}
-        let membership = request.get(options);
-        let membershipId = membership.membershipId;
-        if (this.event.session.user.accessToken == undefined) {
-            this.emit(':tellWithLinkAccountCard',
-                'your id is ' + membershipId);
-            return;
-        }
-        else {
-            this.emit(':tellWithLinkAccountCard',
-            'you are authenticated');
-        return;
-        }
-    },
-    'BetterGear': function () {
-        const speechOutput = this.t('BETTER_GEAR');
-        this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'));
-        // if (this.event.session.user.accessToken == undefined) {
-        //     this.emit(':tellWithLinkAccountCard',
-        //         'to start using this skill, please use the companion app to authenticate on Amazon');
-        //     return;
-        // }
-    },
-    'GetItem': function () {
-        const speechOutput = this.t('GET_FACT_MESSAGE');
-        this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'));
-    },
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = this.t('HELP_MESSAGE');
-        const reprompt = this.t('HELP_MESSAGE');
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'Unhandled': function () {
-        this.emit(':ask', 'HELP_MESSAGE', 'HELP_MESSAGE');
-    },
-};
+var request = require("request")
 
 exports.handler = function (event, context) {
-    const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
 };
+
+function onSessionStarted(sessionStartedRequest, session) {
+    // add any session init logic here
+}
+
+function onLaunch(launchRequest, session, callback) {
+    getWelcomeResponse(callback)
+}
+
+function onIntent(intentRequest, session, callback) {
+
+    var intent = intentRequest.intent
+    var intentName = intentRequest.intent.name;
+
+    // dispatch custom intents to handlers here
+    if (intentName == "TuckerTest") {
+        handleGetInfoIntent(intent, session, callback)
+    } else {
+         throw "Invalid intent"
+    }
+}
+
+function onSessionEnded(sessionEndedRequest, session) {
+
+}
+
+// ------- Skill specific logic -------
+
+function getWelcomeResponse(callback) {
+    var speechOutput = "Welcome! Do you want to hear about some facts?"
+
+    var reprompt = "Do you want to hear about some facts?"
+
+    var header = "Get Info"
+
+    var shouldEndSession = false
+
+    var sessionAttributes = {
+        "speechOutput" : speechOutput,
+        "repromptText" : reprompt
+    }
+
+    callback(sessionAttributes, buildSpeechletResponse(header, speechOutput, reprompt, shouldEndSession))
+
+}
+
+function handleGetInfoIntent(intent, session, callback) {
+
+    var speechOutput = "We have an error"
+
+    getJSON(function(data) {
+        if (data != "ERROR") {
+            var speechOutput = data
+        }
+        callback(session.attributes, buildSpeechletResponseWithoutCard(speechOutput, "", true))
+    })
+
+}
+
+function destinyGetCharacter() {
+    return {
+        url: "https://bungie.net/Platform/User/SearchUsers/?q=alltuckerdout",
+        headers: {
+            'X-API-Key': '461360109a974a24a9f07e54653a5001'
+        }
+    }
+}
+
+function getJSON(callback) {
+    request.get(url2(), function(error, response, body) {
+        var result = JSON.parse(body)
+        if (result) {
+            callback(result.Response[0].displayName)
+        } else {
+            callback("ERROR")
+        }
+    })
+}
+
+
+// ------- Helper functions to build responses for Alexa -------
+
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
+
+function capitalizeFirst(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
